c5e4148636ad8af54d663f5ba2765cc023c4c038
Antonio Chávez López <38500555+antoniochl98@users.noreply.github.com>
2018-04-19 14:00:07 +0100

    practica5

--- funciones_ordenacion.cpp
+++ funciones_ordenacion.cpp
@@ -1,42 +1,33 @@
 
-#include<iostream>
-#include<vector>
-#include<stdio.h>
-#include"dni.hpp"
+#include"funciones_ordenacion.hpp"
 
+extern float const_reduc;
 
 using namespace std;
-/** VARIABLES GLOBALES */
 
-
-
-
-template<class D>
+template<class T>
 /**insercion*/
-  void insertar(vector<D>& sec,bool verbose){
+  void insertar(vector<T>& ordena,bool verbose){
    int i,  j;
-   D x;
+   T x;
 
-   for (i = 1; i < sec.size(); i++){
-       x = sec[i];
+   for (i = 1; i < ordena.size(); i++){
+       x = ordena[i];
        j = i-1;
        if(verbose){
 		   cin.ignore();
 		   cout<<"El elemento a ordenar es la posición "<<i+1<<" el elemento "<<x<<endl;;
 	   }
 
-       while (j >= 0 &&  x < sec[j])
+       while (j >= 0 &&  x < ordena[j])
        {
-           sec[j+1] = sec[j];
+           ordena[j+1] = ordena[j];
            j = j-1;
        }
-       sec[j+1] = x;
+       ordena[j+1] = x;
        if(verbose){
     	   cout<<"El elemento "<<x<<" ha sido colocado en la posición "<<i+1<<endl;
-		   /*for (int k=0;j<sec.size();k++){
-			   cout<<sec[k]<<" ";
-		   }
-		   cout<<endl;*/
+
        }
    }
   }
@@ -44,27 +35,27 @@ template<class D>
 
 /**Por Intercambio*/
 template<class T>
-  void burbuja(vector<T>& sec, bool verbose){
-    bool flag=false;
-    for (int i = 1; i < sec.size() && (flag==false); i++){
-      flag=true;
+  void burbuja(vector<T>& ordena, bool verbose){
+    bool no_cambio=false;
+    for (int i = 1; i < ordena.size() && (no_cambio==false); i++){
+      no_cambio=true;
       int j=0;
-      for (j = sec.size()-1; j >= i ; j--){//las pasadas
-        if (sec[j] < sec[j-1] ){
+      for (j = ordena.size()-1; j >= i ; j--){
+        if (ordena[j] < ordena[j-1] ){
 
-          flag=false;
-          T x = sec[j-1] ;
-          sec[j-1] = sec[j] ;
-          sec[j] = x ;
+          no_cambio=false;
+          T x = ordena[j-1] ;
+          ordena[j-1] = ordena[j] ;
+          ordena[j] = x ;
 
         }
 
       }
       if(verbose==true){
-		cout<<"Insertando el elemento burbuja "<<sec[j]<<endl;
-		for(int k=0;k<sec.size();k++){
+		cout<<"Insertando el elemento burbuja "<<ordena[j]<<endl;
+		for(int k=0;k<ordena.size();k++){
 		  cout<<" ";
-		  cout<<sec[k];
+		  cout<<ordena[k];
 		}
 		cout<<endl;
       }
@@ -75,75 +66,98 @@ template<class T>
 
 template<class H>
 
-  void heapSort(vector<H>& sec, bool verbose){
-	int n=sec.size();
+  void heapSort(vector<H>& ordena, bool verbose){
+	int n=ordena.size();
     H x;
     for (int i = (n-1)/2; i >= 0; i--){
-      baja(i, sec, n);
+      baja(i, ordena, n);
     }
     for (int i = (n-1); i >= 1; i--){
-      x = sec[0] ;
-      sec[0] = sec[i] ;
-      sec[i] = x ;
-      baja(0,sec,i-1) ;
+      x = ordena[0] ;
+      ordena[0] = ordena[i] ;
+      ordena[i] = x ;
+      baja(0,ordena,i-1) ;
     }
   }
-template<class H>
-  void baja( int i , vector<H>& sec, int n){
-    H x = sec[i] ;
+template<class T>
+  void baja( int i , vector<T>& ordena, int n){
+    T x = ordena[i] ;
     int h;
     while ( ((2*i)+1) < n){
       int h1 = ((2*i)+1);
       int h2 = h1 + 1 ;
-      if (h1 == n){
-         h = h2;
+      if (h1 == n-1){
+         h = h1;
       }
-      else if (sec[h2] < sec[h1]){
+      else if (ordena[h2] < ordena[h1]){
         h = h1;
       }
       else h = h2 ;
-      if (sec[h] <= x){
+      if (ordena[h] <= x){
         break ;
       }
       else{
-        sec[i] = sec[h];
-        sec[h] = x ;
+        ordena[i] = ordena[h];
+        ordena[h] = x ;
         i = h;
       }
     }
   }
 
-  template<class H>
-
-    void Swap(H& a, H& b){
-      H aux;
+  template<class T>
+    void Swap(T& a, T& b){
+      T aux;
       aux= a;
       a=b;
       b=aux;
     }
 
-  template<class E>
+  template<class T>
 
-  void Qsort( vector<E>& sec, int ini, int fin){
+  void Qsort( vector<T>& ordena, int ini, int fin){
     int i = ini , f = fin ;
-    E p = sec[(i+f)/2] ;
+    T p = ordena[(i+f)/2] ;
     while (i <= f){
-      while (sec[i] < p) i++ ;
-      while (sec[f] > p) f-- ;
+      while (ordena[i] < p) i++ ;
+      while (ordena[f] > p) f-- ;
       if (i <= f){
-        E x = sec[i] ;
-        sec[i] = sec[f] ;
-        sec[f] = x ;
+        T x = ordena[i] ;
+        ordena[i] = ordena[f] ;
+        ordena[f] = x ;
         i++ ; f-- ;
       } ;
     } ;
-    if (ini < f) Qsort(sec, ini, f) ;
-    if (i < fin) Qsort(sec, i, fin) ;
+    if (ini < f) Qsort(ordena, ini, f) ;
+    if (i < fin) Qsort(ordena, i, fin) ;
   }
 
-  template<class E>
+  template<class T>
 
-  void Quicksort(vector<E>& sec, bool verbose){
-	  Qsort(sec,0,sec.size()-1);
-	  
+  void Quicksort(vector<T>& ordena, bool verbose){
+	  Qsort(ordena,0,ordena.size()-1);
   }
+
+
+  template<class T>
+
+     void shell(vector<T>& ordena,bool verbose){
+         int del = ordena.size();
+         while (del > 1){
+             del = del * const_reduc;
+             shellsort(del,ordena) ;
+         }
+     }
+   template<class T>
+     void shellsort(int d , vector<T>& ordena){
+         T x;
+         int j;
+         for (int i = d + 1; i <= ordena.size(); i++){
+             x = ordena[i] ;
+             j = i ;
+             while ((j > d) && (x < ordena[j-d])){
+                 ordena[j] = ordena[j-d] ;
+                 j = j - d ;
+             }
+             ordena[j] = x ;
+         }
+     }
